90c0bed77d85f8c833a5386d11e36b0836ca114c
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-01 17:31:34 +0600

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: { origins: ["http://localhost:3000"] },
+});
+const cors = require("cors");
+const fs = require("fs"),
+  path = require("path");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+// function startTimer(params) {
+//   var clearTime = setInterval(() => {
+//     io.emit("timer", i++);
+//     if (i > 10) {
+//       i = 0;
+//     }
+//   }, 1000);
+// }
+
+let message = [];
+
+setInterval(() => {
+  message = [];
+  console.log("Array Emptied");
+  let msg = "Chat Deleted";
+  function alert(msg) {
+    io.emit("alert", msg);
+    io.emit("chat message", message);
+  }
+  alert(msg);
+  setTimeout(() => {
+    alert(null);
+  }, 3000);
+}, 600000);
+
+let i = 0;
+
+io.on("connection", (socket) => {
+  io.emit("chat message", message);
+
+  console.log(socket.id);
+  const readStream = fs.createReadStream(path.resolve(__dirname, "./img.jpg"), {
+      encoding: "binary",
+    }),
+    chunks = [];
+  let delay = 0;
+  readStream.on("data", function (chunk) {
+    chunks.push(chunk);
+
+    socket.emit("img-chunk", chunk);
+  });
+  socket.on("chat message", (msg) => {
+    message.push(msg);
+    console.log(message);
+    io.emit("chat message", message);
   });
 });
 
