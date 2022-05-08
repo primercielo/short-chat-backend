4d28f2919cabcb1b6bc59c8e8a1f0f03c6c73a76
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-09 00:59:06 +0600

    cors fixing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 
 const io = require("socket.io")(http, {
-  cors: { origins: ["https://short-chat.vercel.app/"] },
+  cors: { origins: ["https://short-chat.vercel.app"] },
 });
 
 const cors = require("cors");
