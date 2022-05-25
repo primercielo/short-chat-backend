6b33b700dd2752cd3a614f97540d82722bc601e9
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-25 08:18:03 +0600

    Social: social table destroy

--- app/routes/social.protected.js
+++ app/routes/social.protected.js
@@ -3,5 +3,5 @@ const { auth } = require("../middleware");
 module.exports = (app) => {
   app.post("/social", social.createPost);
   app.get("/socials/:limit", social.getPosts);
-  //   app.get("/social/destroy", social.socialDeleteAllPost);
+  app.get("/social/destroy", social.socialDeleteAllPost);
 };
