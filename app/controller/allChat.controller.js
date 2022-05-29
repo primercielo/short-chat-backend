d7721acf4d27cf419caf1fab918dc6836afdaace
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2022-05-30 00:25:10 +0600

    Get All Chat with Raw SQL coding with ORDER on LIMIT

--- app/controller/allChat.controller.js
+++ app/controller/allChat.controller.js
@@ -3,12 +3,19 @@ const Chat = db.Chat;
 
 exports.getAllChat = async (req, res) => {
   try {
-    const response = await Chat.findAndCountAll({
-      order: [["createdAt", "DESC"]],
-      offset: 0,
-      limit: req.params.limit,
-    });
-    res.status(200).send(response);
+    // const response = await Chat.findAndCountAll({
+    //   order: [["createdAt", "DESC"]],
+    //   offset: 0,
+    //   limit: req.params.limit,
+    // });
+    let chats = {};
+    const response = await db.sequelize
+      .query(`SELECT * FROM (SELECT * FROM public."Chats"
+ORDER BY id DESC
+LIMIT ${req.params.limit}) a ORDER BY id ASC`);
+    chats.count = response[1].rowCount;
+    chats.rows = response[1].rows;
+    res.status(200).send(chats);
   } catch (error) {
     res.status(200).send({ message: "Error! while in getAllChat()" });
   }
--- index.js
+++ index.js
@@ -28,8 +28,8 @@ var corsOptions = {
 };
 
 // middleware
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/peerjs", peerServer);
 
